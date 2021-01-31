3ffd8e46b76ba94e1edb74eefb689f82d11510ba
ermiry <erick.salas@ermiry.com>
2021-01-31 02:54:07 -0600

    Added base logic to listen & accept requests

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,3 +1,9 @@
+use std::io::Read;
+use std::convert::TryFrom;
+use std::net::TcpListener;
+
+use crate::http::Request;
+
 pub struct Server {
 	address: String
 }
@@ -10,6 +16,34 @@ impl Server {
 	}
 
 	pub fn run(self) {
+		let listener = TcpListener::bind(&self.address).unwrap();
+
 		println!("Listening on {}", self.address);
+
+		loop {
+			match listener.accept() {
+				Ok((mut stream, addr)) => {
+					println!("new client: {:?}", addr);
+
+					let mut buffer = [0; 4096];
+					match stream.read(&mut buffer) {
+						Ok(size) => {
+							println!(
+								"Received a request ({}): {:?}",
+								size,
+								String::from_utf8_lossy(&buffer)
+							);
+
+							Request::try_from(&buffer[..]);
+						},
+						Err(e) => {
+							println!("failed to read from connection: {:?}", e)
+						}
+					}
+				},
+
+				Err(e) => println!("couldn't get client: {:?}", e)
+			}
+		}
 	}
 }
